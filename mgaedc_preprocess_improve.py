84922eb25918964a3f5fb6287224bb10c58a7f8a
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-04-09 17:19:38 -0400

    bug fix

--- mgaedc_preprocess_improve.py
+++ mgaedc_preprocess_improve.py
@@ -121,10 +121,12 @@ def run(params: Dict):
 
     ##
     # drop from features if no response value
-    cell_feat = cell_feat.reset_index()
-    cell_feat = cell_feat[cell_feat[params['cell_column_name']].isin(response_all[params['cell_column_name']].to_list())]
-    drug_feat_raw = drug_feat_raw.reset_index()
-    drug_feat_raw = drug_feat_raw[drug_feat_raw[params['drug_column_name']].isin(drugslist)]
+    #cell_feat = cell_feat.reset_index()
+    cell_feat = cell_feat.loc[cellslist]
+    #cell_feat = cell_feat[cell_feat[params['cell_column_name']].isin(response_all[params['cell_column_name']].to_list())]
+    #drug_feat_raw = drug_feat_raw.reset_index()
+    drug_feat_raw = drug_feat_raw.loc[drugslist]
+    #[drug_feat_raw[params['drug_column_name']].isin(drugslist)]
 
 
     # Convert to sparse matrix
